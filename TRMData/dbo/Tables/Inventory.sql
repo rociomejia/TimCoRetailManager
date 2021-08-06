83c14aad19c190d1946d81a4022680d226b48789
Rocio <chiocjava@yahoo.es>
2021-08-06 13:53:29 -0600

    Register DB tables for the basic register system

---
+++ TRMData/dbo/Tables/Inventory.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity, 
+    [ProductId] INT NOT NULL, 
+    [Quantity] INT NOT NULL, 
+    [PurchasePrice] MONEY NOT NULL, 
+    [PurchaseDate] DATETIME2 NULL DEFAULT getutcdate()
+)
---
+++ TRMData/dbo/Tables/Product.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProductName] NVARCHAR(100) NOT NULL, 
+    [Description] NVARCHAR(MAX) NOT NULL, 
+	[RetailPrice] money not null,
+    [CreateDate] DATETIME2 NULL DEFAULT getutcdate(), 
+    [LastModified] DATETIME2 NULL DEFAULT getutcdate()
+)
